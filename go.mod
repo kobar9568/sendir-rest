143e85b7d75d38194f2e9b61644c3af08aa89970
Ryota Kobayashi <38117745+kobar9568@users.noreply.github.com>
2021-05-03 07:52:40 +0900

    feat: Create Web server.

--- go.mod
+++ go.mod
@@ -1,3 +1,5 @@
 module github.com/kobar9568/sendir-rest
 
 go 1.16
+
+require github.com/labstack/echo/v4 v4.2.2
--- main.go
+++ main.go
@@ -1,7 +1,29 @@
 package main
 
-import "fmt"
+import (
+	"net/http"
+
+	"github.com/labstack/echo/v4"
+	"github.com/labstack/echo/v4/middleware"
+)
+
+const PORT = ":3001"
 
 func main() {
-	fmt.Println("sendir-rest")
+	e := echo.New()
+
+	e.Use(middleware.Logger())
+	e.Use(middleware.Recover())
+
+	e.GET("/api/v1/test", test)
+
+	e.Logger.Fatal(e.Start(PORT))
+}
+
+func test(c echo.Context) error {
+	type response struct {
+		Message string `json:"message"`
+	}
+
+	return c.JSON(http.StatusOK, &response{Message: "test"})
 }
